db578ae5b8d2ab383e75a586b98755605f9c7d58
marin <marin@marindeMacBook-Pro.local>
2020-09-28 03:44:07 +0800

    update all feature for r1

--- src/test/java/cn/second_hand/UnitTest.java
+++ src/test/java/cn/second_hand/UnitTest.java
@@ -2,10 +2,8 @@ package cn.second_hand;
 
 import org.junit.Test;
 
-import cn.second_hand.cart.dao.CartDao;
-import cn.second_hand.product.dao.OrderDao;
-import cn.second_hand.query.AuditQueryObject;
 import cn.second_hand.user.dao.UserDao;
+import cn.second_hand.user.domain.User;
 import cn.second_hand.user.utils.MongoDBUtils;
 
 public class UnitTest {
@@ -13,38 +11,38 @@ public class UnitTest {
 	MongoDBUtils mangoDb;
 	
 	UserDao userDao = new UserDao();
-	
-	CartDao carDao = new CartDao();
-	
-	OrderDao orderDao = new OrderDao();
-	
-//	@Test
-//	public void testMangoDBConnection() {
-//		mangoDb.getConnect();
-//	}
-	
-//	@Test
-//	public void testFindByEmail() {
-//		userDao.findByEmail("574811625@qq.com");
-//	}
 //	
-//	@Test
-//	public void testRegister() {
-//		User user =new User();
-//		user.setEmail("123@gmail.com");
-//		user.setActiveStatus(true);
-//		user.setPassword("123456");
-//		user.setVerifyCode("123");
-//		userDao.register(user);
-//	}
+//	CartDao carDao = new CartDao();
+//	
+//	OrderDao orderDao = new OrderDao();
+	
+	@Test
+	public void testMangoDBConnection() {
+		mangoDb.getConnect();
+	}
+	
+	@Test
+	public void testFindByEmail() {
+		userDao.findByEmail("574811625@qq.com");
+	}
+	
+	@Test
+	public void testRegister() {
+		User user =new User();
+		user.setEmail("123@gmail.com");
+		user.setActiveStatus(true);
+		user.setPassword("123456");
+		user.setVerifyCode("123");
+		userDao.register(user);
+	}
 //	
 //	@Test
 //	public void testupdateActiveState() {
 //		userDao.updateActiveState("123", true);
 //	}
 	
-	@Test
-	public void testgetSellerOrderList() {
-		orderDao.getSellerOrderList(new AuditQueryObject());
-	}
+//	@Test
+//	public void testgetSellerOrderList() {
+//		orderDao.getSellerOrderList(new AuditQueryObject());
+//	}
 }
