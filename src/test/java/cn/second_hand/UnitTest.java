a39a8abd84446f6380e231b93fc1109a3b6c3843
marin <marin@marindeMacBook-Pro.local>
2020-09-28 02:15:23 +0800

    Automated Testing

--- src/test/java/cn/second_hand/UnitTest.java
+++ src/test/java/cn/second_hand/UnitTest.java
@@ -17,23 +17,23 @@ public class UnitTest {
 		mangoDb.getConnect();
 	}
 	
-//	@Test
-//	public void testFindByEmail() {
-//		userDao.findByEmail("574811625@qq.com");
-//	}
-//	
-//	@Test
-//	public void testRegister() {
-//		User user =new User();
-//		user.setEmail("123@gmail.com");
-//		user.setActiveStatus(true);
-//		user.setPassword("123456");
-//		user.setVerifyCode("123");
-//		userDao.register(user);
-//	}
-//	
-//	@Test
-//	public void testupdateActiveState() {
-//		userDao.updateActiveState("123", true);
-//	}
+	@Test
+	public void testFindByEmail() {
+		userDao.findByEmail("574811625@qq.com");
+	}
+	
+	@Test
+	public void testRegister() {
+		User user =new User();
+		user.setEmail("123@gmail.com");
+		user.setActiveStatus(true);
+		user.setPassword("123456");
+		user.setVerifyCode("123");
+		userDao.register(user);
+	}
+	
+	@Test
+	public void testupdateActiveState() {
+		userDao.updateActiveState("123", true);
+	}
 }
