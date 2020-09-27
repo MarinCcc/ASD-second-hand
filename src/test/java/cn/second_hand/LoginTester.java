a095d995a25439159e7fffd4d3b11c28f317a536
marin <marin@marindeMacBook-Pro.local>
2020-09-28 02:11:33 +0800

    Automated Testing

--- src/test/java/cn/second_hand/LoginTester.java
+++ src/test/java/cn/second_hand/LoginTester.java
@@ -7,7 +7,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LoginTester {
-	@Test
 	@Given("^user navigates to login.jsp page$")
 	public void navigatePage() {
 	System.out.println("Cucumber executed Given statement");
@@ -20,19 +19,16 @@ public class LoginTester {
 	System.out.println("Password is:"+ password);
 	}
 
-	@Test
 	@When("^click the Submit button$")
 	public void clickTheSubmitButton() {
 	System.out.println("Executing When statement");
 	}
 
-	@Test
 	@Then("^Home page should be displayed$")
 	public void validatePage() {
 	System.out.println("Executing Then statement");
 	}
 	
-	@Test
 	@Then("^login should be successful$")
 	public void validateLoginSuccess() {
 	System.out.println("Executing 2<sup>nd</sup> Then statement");
