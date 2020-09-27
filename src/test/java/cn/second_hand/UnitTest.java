89bf06c504c782ff0a8fcba631387dccb337b893
marin <marin@marindeMacBook-Pro.local>
2020-09-28 02:38:14 +0800

    Automated Testing

--- src/test/java/cn/second_hand/UnitTest.java
+++ src/test/java/cn/second_hand/UnitTest.java
@@ -2,8 +2,9 @@ package cn.second_hand;
 
 import org.junit.Test;
 
+import cn.second_hand.cart.dao.CartDao;
+import cn.second_hand.cart.domain.Cart;
 import cn.second_hand.user.dao.UserDao;
-import cn.second_hand.user.domain.User;
 import cn.second_hand.user.utils.MongoDBUtils;
 
 public class UnitTest {
@@ -12,28 +13,35 @@ public class UnitTest {
 	
 	UserDao userDao = new UserDao();
 	
+	CartDao carDao = new CartDao();
+	
 //	@Test
 //	public void testMangoDBConnection() {
 //		mangoDb.getConnect();
 //	}
 	
-	@Test
-	public void testFindByEmail() {
-		userDao.findByEmail("574811625@qq.com");
-	}
+//	@Test
+//	public void testFindByEmail() {
+//		userDao.findByEmail("574811625@qq.com");
+//	}
+//	
+//	@Test
+//	public void testRegister() {
+//		User user =new User();
+//		user.setEmail("123@gmail.com");
+//		user.setActiveStatus(true);
+//		user.setPassword("123456");
+//		user.setVerifyCode("123");
+//		userDao.register(user);
+//	}
+//	
+//	@Test
+//	public void testupdateActiveState() {
+//		userDao.updateActiveState("123", true);
+//	}
 	
 	@Test
-	public void testRegister() {
-		User user =new User();
-		user.setEmail("123@gmail.com");
-		user.setActiveStatus(true);
-		user.setPassword("123456");
-		user.setVerifyCode("123");
-		userDao.register(user);
-	}
-	
-	@Test
-	public void testupdateActiveState() {
-		userDao.updateActiveState("123", true);
+	public void testsaveCart() {
+		carDao.saveCart(new Cart());
 	}
 }
