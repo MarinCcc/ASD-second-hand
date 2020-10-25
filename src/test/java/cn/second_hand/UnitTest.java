efd3e686f8d7cca756da31fdeefb7559302e22c9
Mark Lin <mark727221029@gmail.com>
2020-10-25 15:05:42 +1100

    Automated testing

--- src/test/java/cn/second_hand/UnitTest.java
+++ src/test/java/cn/second_hand/UnitTest.java
@@ -1,48 +1,55 @@
 package cn.second_hand;
 
+import org.bson.types.ObjectId;
 import org.junit.Test;
 
-import cn.second_hand.user.dao.UserDao;
-import cn.second_hand.user.domain.User;
+import cn.second_hand.category.dao.CategoryDao;
+import cn.second_hand.category.domain.Category;
 import cn.second_hand.user.utils.MongoDBUtils;
 
 public class UnitTest {
-	
-	MongoDBUtils mangoDb;
-	
-	UserDao userDao = new UserDao();
-//	
-//	CartDao carDao = new CartDao();
-//	
-//	OrderDao orderDao = new OrderDao();
+
+   MongoDBUtils mangoDb;
+   CategoryDao dao = new CategoryDao();
+    
 	
 	@Test
 	public void testMangoDBConnection() {
 		mangoDb.getConnect();
 	}
 	
-	@Test
-	public void testFindByEmail() {
-		userDao.findByEmail("574811625@qq.com");
-	}
 	
 	@Test
-	public void testRegister() {
-		User user =new User();
-		user.setEmail("123@gmail.com");
-		user.setActiveStatus(true);
-		user.setPassword("123456");
-		user.setVerifyCode("123");
-		userDao.register(user);
+	public void testAdd() {
+		
+		Category category = new Category();
+	    category.setCategoriesName("python");
+	    category.setDescription("this is a catrgory for python");
+	    dao.add(category);
+
 	}
-//	
-//	@Test
-//	public void testupdateActiveState() {
-//		userDao.updateActiveState("123", true);
-//	}
 	
-//	@Test
-//	public void testgetSellerOrderList() {
-//		orderDao.getSellerOrderList(new AuditQueryObject());
-//	}
+	
+	@Test
+	public void testEdit() {
+
+		Category category = new Category();
+		ObjectId oid = category.getOid();
+	    category.setCategoriesName("java2");
+	    category.setDescription("this is a catrgory for java2");
+	    dao.edit(category);
+		
+	
+	}
+	
+	
+	@Test
+	public void testfindAll(){
+		
+		dao.findAll();
+		
+		
+	}
+
+
 }
